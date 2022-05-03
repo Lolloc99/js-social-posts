b2a8d604745239078463cd4351ca348c5b13f7b0
Lolloc99 <lolloc99@yahoo.it>
2022-05-03 15:03:48 +0200

    Milestone 1 & 2 aggiunte, da adattare i dati

--- js/script.js
+++ js/script.js
@@ -37,15 +37,136 @@ console.log() è nostro amico.
 const posts = [
     {
         "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.", // testo del post
+        "media": "https://unsplash.it/600/300?image=171", //immagine post
 
         "author": {
             "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
+            "image": "https://unsplash.it/300/300?image=15" // immagine utente
         },
 
         "likes": 80,
         "created": "2021-06-25"
+    },
+
+    {
+        "id": 2,
+        "content": "Ma fossi tu quel barbaro barbiere che barbassi quella barba così barbaramente a piazza Barberini.", // testo del post
+        "media": "https://unsplash.it/600/300?image=172", //immagine post
+
+        "author": {
+            "name": "Giacomo Antonelli",
+            "image": "https://unsplash.it/300/300?image=16" // immagine utente
+        },
+
+        "likes": 128,
+        "created": "2021-09-17"
+    },
+
+    {
+        "id": 3,
+        "content": " Se l’arcivescovo di Costantinopoli si disarcivescoviscostantinopolizzasse, tu ti disarcivescoviscostantinopolizzeresti come si è disarcivescoviscostantinopolizzato l’arcivescovo di Costantinopoli?", // testo del post
+        "media": "https://unsplash.it/600/300?image=173", //immagine post
+
+        "author": {
+            "name": "Julia Alexorf",
+            "image": "https://unsplash.it/300/300?image=17" // immagine utente
+        },
+
+        "likes": 46,
+        "created": "2021-02-29"
+    },
+
+    {
+        "id": 4,
+        "content": "Al pozzo di Santa Pazzia protettrice dei pazzi, c'è una pazza che lava una pezza, arriva un pazzo con un pezzo di pizza e chiede alla pazza se ne vuole un pezzo, la pazza rifiuta il pazzo si infuria e getta la pazza, la pezza e la pizza nel pozzo di Santa Pazzia protettrice dei pazzi", // testo del post
+        "media": "https://unsplash.it/600/300?image=174", //immagine post
+
+        "author": {
+            "name": "Arianna Easteregg",
+            "image": "https://unsplash.it/300/300?image=18" // immagine utente
+        },
+
+        "likes": 230,
+        "created": "2021-1-8"
     }
 ];
+
+/* Esempio post
+<!-- post di esempio/template, da togliere/commentare e generare da JS -->
+
+    <div class="post">
+        <div class="post__header"> <!-- Sezione Header della card -->
+            <div class="post-meta">                    
+                <div class="post-meta__icon"> <!-- Immagine profilo -->
+                    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data"> <!-- Info profilo -->
+                    <div class="post-meta__author">Phil Mangione</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div> <!-- Testo del post -->
+        <div class="post__image"> <!-- Immagine del post -->
+            <img src="https://unsplash.it/600/300?image=171" alt="">
+        </div>
+        <div class="post__footer"> <!-- Sezione Footer della card -->
+            <div class="likes js-likes">
+                <div class="likes__cta"> <!-- tasto mi piace -->
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+*/
+const container = document.getElementById("container");
+
+posts.forEach(element => {
+    divMaker(container)
+});
+
+
+// Creare elementi da appendere poi a "container"
+function divMaker(container) {
+    
+    const divPostCard = document.createElement("div");
+        divPostCard.classList.add("post");
+        divPostCard.innerHTML = 
+        `<div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data"> <!-- Info profilo -->
+                    <div class="post-meta__author">Phil Mangione</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+        <div class="post__image">
+            <img src="https://unsplash.it/600/300?image=171" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                </div>
+            </div> 
+        </div>`
+
+    container.append(divPostCard)
+}
